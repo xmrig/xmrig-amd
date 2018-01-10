35df5c8ccd7befbb577f66535e01e75a1afbd05f
XMRig <support@xmrig.com>
2018-01-10 18:23:54 +0700

    Sync changes with xmrig miner.
    * Add libmicrohttpd version to --version output.
    * Fix recent MSVC 2017 version detection.
    * Remove extra semicolon.
    * Reduce cmake version requirement to 2.8.

--- src/Options.cpp
+++ src/Options.cpp
@@ -4,7 +4,7 @@
  * Copyright 2014      Lucas Jones <https://github.com/lucasjones>
  * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
- * Copyright 2016-2017 XMRig       <support@xmrig.com>
+ * Copyright 2016-2018 XMRig       <support@xmrig.com>
  *
  *
  *   This program is free software: you can redistribute it and/or modify
@@ -30,6 +30,11 @@
 #   include "getopt/getopt.h"
 #else
 #   include <getopt.h>
+#endif
+
+
+#ifndef XMRIG_NO_HTTPD
+#   include <microhttpd.h>
 #endif
 
 
@@ -753,8 +758,9 @@ void Options::showVersion()
 
     printf("\nlibuv/%s\n", uv_version_string());
 
-//     const int cudaVersion = cuda_get_runtime_version();
-//     printf("CUDA/%d.%d\n", cudaVersion / 1000, cudaVersion % 100);
+#   ifndef XMRIG_NO_HTTPD
+    printf("libmicrohttpd/%s\n", MHD_get_version());
+#   endif
 }
 
 
