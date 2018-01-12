c46ba3102bd2c7a0b624fe5cac8a2df6a359c479
XMRig <support@xmrig.com>
2018-01-12 18:05:20 +0700

    v2.4.4

--- src/version.h
+++ src/version.h
@@ -27,7 +27,7 @@
 #define APP_ID        "xmrig"
 #define APP_NAME      "XMRig"
 #define APP_DESC      "XMRig OpenCL miner"
-#define APP_VERSION   "2.4.3-beta3"
+#define APP_VERSION   "2.4.4"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2018 xmrig.com"
@@ -35,8 +35,8 @@
 
 #define APP_VER_MAJOR  2
 #define APP_VER_MINOR  4
-#define APP_VER_BUILD  3
-#define APP_VER_REV    3
+#define APP_VER_BUILD  4
+#define APP_VER_REV    0
 
 #ifdef _MSC_VER
 #   if (_MSC_VER >= 1910)
