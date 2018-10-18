bd7c826873ffbffb461c84fab7ebde05efb6b5cb
XMRig <support@xmrig.com>
2018-10-18 10:47:45 +0700

    v2.8.2

--- src/version.h
+++ src/version.h
@@ -27,7 +27,7 @@
 #define APP_ID        "xmrig-amd"
 #define APP_NAME      "XMRig-AMD"
 #define APP_DESC      "XMRig OpenCL miner"
-#define APP_VERSION   "2.8.2-dev"
+#define APP_VERSION   "2.8.2"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2018 xmrig.com"
