73a57ca2821492236efb5a1a79a7d1d2783f80ab
MoneroOcean <support@moneroocean.stream>
2018-10-21 10:00:56 +0200

    Fixed stride warning

--- src/workers/Workers.cpp
+++ src/workers/Workers.cpp
@@ -283,7 +283,7 @@ bool Workers::switch_algo(const xmrig::Algorithm& algorithm)
 
     contexts.resize(m_threadsCount);
 
-    const bool isCNv2 = algorithm.algo() == xmrig::CRYPTONIGHT;
+    const bool isCNv2 = algorithm.algo() == xmrig::CRYPTONIGHT && algorithm.variant() == xmrig::VARIANT_2;
     for (size_t i = 0; i < m_threadsCount; ++i) {
         const OclThread *thread = static_cast<OclThread *>(threads[i]);
         if (isCNv2 && thread->stridedIndex() == 1) {
