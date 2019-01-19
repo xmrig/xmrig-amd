21d0f30c264d295635e1184c50caaf2a65e4d3d4
XMRig <support@xmrig.com>
2019-01-19 15:14:58 +0700

    Sync changes.

--- src/workers/OclWorker.h
+++ src/workers/OclWorker.h
@@ -6,8 +6,8 @@
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
  * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
  * Copyright 2018      Lee Clagett <https://github.com/vtnerd>
- * Copyright 2018      SChernykh   <https://github.com/SChernykh>
- * Copyright 2016-2018 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright 2018-2019 SChernykh   <https://github.com/SChernykh>
+ * Copyright 2016-2019 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -25,6 +25,7 @@
 
 #ifndef XMRIG_OCLWORKER_H
 #define XMRIG_OCLWORKER_H
+
 
 #include <atomic>
 
@@ -71,7 +72,7 @@ private:
     uint32_t m_pausedNonce;
     uint64_t m_count;
     uint64_t m_sequence;
-    uint8_t m_blob[96]; // Max blob size is 84 (75 fixed + 9 variable), aligned to 96. https://github.com/xmrig/xmrig/issues/1 Thanks fireice-uk.
+    uint8_t m_blob[Job::kMaxBlobSize];
 };
 
 
